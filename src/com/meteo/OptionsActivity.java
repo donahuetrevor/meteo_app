8d3e44363cb7b2e3c8f46c93ba1cd7f9e1037ed5
Dan Cirnat <dan@alt.md>
2012-11-11 16:50:44 +0200

    options stub

--- src/com/meteo/OptionsActivity.java
+++ src/com/meteo/OptionsActivity.java
@@ -1,7 +1,14 @@
 package com.meteo;
 
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import com.meteo.R;
+
 
 public class OptionsActivity extends PreferenceActivity{
-    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.options);
+    }
 }
